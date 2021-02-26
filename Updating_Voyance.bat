623ceed58d400bd5398b1255abd83b6819ec9277
MavenImaging <79724514+MavenImaging@users.noreply.github.com>
2021-02-26 16:15:52 -0500

    Uploading two files

---
+++ Updating_Voyance.bat
@@ -0,0 +1,15 @@
+:: Create MTU Path
+Powershell New-Item -Path 'C:\MTU' -ItemType Directory
+
+:: Downloads the Dealer.pkg branding package into the MTU directory
+Powershell Invoke-WebRequest https://www.dropbox.com/s/5bqldroc4x86ree/Dealer.pkg?dl=1 -OutFile C:\MTU\Dealer.pkg 
+
+:: Downloads Voyance into the MTU Directory
+Powershell Invoke-WebRequest http://medicatechusa.com/wp-content/uploads/voyance/Voyance_v1.3.1612.2946_cfc4ebb3_Setup.exe -OutFile C:\MTU\Voyance_v1.3.1612.2946_cfc4ebb3_Setup.exe
+
+:: Installs Voyance passing the silent argument parameter
+start /wait C:\MTU\Voyance_v1.3.1612.2946_cfc4ebb3_Setup.exe /SILENT /VERYSILENT
+
+:: Deletes the wifi image
+del "C:\Program Files\Voyance\Resources\Images\wi-fi-disc.png" /s /f /q
+
